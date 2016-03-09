9ef3d76a7ef6f365ff730fc987e8e606626c59bd
vkoba <vkoba@at-consulting.ru>
2016-03-10 01:24:03 +0300

    Initial commit

---
+++ src/main/java/ru/vkoba/exercise/Calculator.kt
@@ -0,0 +1,65 @@
+package ru.vkoba.exercise
+
+import java.util.*
+
+/**
+ * Hello world!
+
+ */
+class Calculator {
+
+}
+
+
+class ReversePolishNotationConverter() {
+    val operations = mutableListOf(
+            Operation('+', false, 1),
+            Operation('-', false, 1),
+            Operation('*', false, 2),
+            Operation('/', false, 3)
+    )
+
+    fun toReversePolishNotation(expr: String): String {
+        val opsStack = Stack<Char>();
+        var result = ""
+        for (symbol in expr) {
+            if (!isOperation(symbol) && !isBracket(symbol)) {
+                result += symbol
+                continue
+            }
+            if (symbol == '(') {
+                opsStack.push(symbol)
+                continue
+            }
+            if (symbol == ')') {
+                while (opsStack.peek() != '(') {
+                    result += " " + opsStack.pop()
+                }
+                //Выталкиваем открывающуюся скобку
+                opsStack.pop()
+                continue
+            }
+            result += " "
+            while (opsStack.isNotEmpty() && priority(symbol) <= priority(opsStack.peek())) {
+                result += " " + opsStack.pop()
+            }
+            opsStack.push(symbol)
+        }
+        while (opsStack.isNotEmpty()) {
+            result += " " + opsStack.pop()
+        }
+        return result.trimIndent()
+    }
+
+    private fun priority(op: Char): Byte = operations.findLast { it -> it.symbol == op }?.priopity ?: throw RuntimeException("Unknown operation!")
+
+
+    fun addOperation(op: Operation) {
+        operations.add(op)
+    }
+
+    private fun isBracket(symbol: Char): Boolean = "()".contains(symbol)
+    private fun isOperation(symbol: Char): Boolean = operations.filter { it -> it.symbol == symbol }.isNotEmpty()
+}
+
+data class Operation(val symbol: Char, val isUnary: Boolean, val priopity: Byte)
---
+++ src/test/java/ru/vkoba/exercise/ReversePolishConverterTest.kt
@@ -0,0 +1,27 @@
+package ru.vkoba.exercise
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+/**
+ * Unit test for simple App.
+ */
+class ReversePolishConverterTest {
+    private val rpnc = ReversePolishNotationConverter()
+
+    @Test
+    fun sum_2and2() {
+        assertEquals("2 2 +", rpnc.toReversePolishNotation("2+2"))
+    }
+
+    @Test
+    fun sum_2and2and3() {
+        assertEquals("2 2 + 3 +", rpnc.toReversePolishNotation("2+2+3"))
+    }
+
+    @Test
+    fun sum_10and100() {
+        assertEquals("10 100 +", rpnc.toReversePolishNotation("10+100"))
+    }
+}
